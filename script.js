c6451289f3f60baaa45bda08b208ea1db20e3a9d
THIAGO DE ALMEIA TELES <thiagofoztt@gmail.com>
2026-03-17 20:48:20 -0300

    Refactor project view button and scroll animation

--- script.js
+++ script.js
@@ -2,14 +2,11 @@
 function verProjeto(url){
 window.open(url, '_blank');
 }
-
 // ANIMAÇÃO AO ROLAR
 const elementos = document.querySelectorAll('.hidden');
-
 window.addEventListener('scroll', () => {
 elementos.forEach(el => {
 const posicao = el.getBoundingClientRect().top;
-
 if(posicao < window.innerHeight){
 el.classList.add('show');
 }
